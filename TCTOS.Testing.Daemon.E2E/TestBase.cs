baade2e56d156a53ecbe99309839ae2ce2747eab
Jonas Mohr <git@al1as.me>
2026-05-10 20:24:59 +0200

    Added safeguard to E2E tests

--- TCTOS.Testing.Daemon.E2E/TestBase.cs
+++ TCTOS.Testing.Daemon.E2E/TestBase.cs
@@ -1,5 +1,6 @@
 using TCTOS.Abstractions;
 using TCTOS.Impls.Local;
+using Xunit.Sdk;
 
 namespace TCTOS.Testing.Daemon.E2E;
 
@@ -10,6 +11,9 @@ public abstract class TestBase : IDisposable
 
     protected TestBase()
     {
+        if (Environment.GetEnvironmentVariable("TCTOS_E2E") == null)
+            throw new XunitException(
+                "Aborted tests because the environment variable \"TCTOS_E2E\" is not set. This is a safeguard against accidental test runs. Running the E2E tests WILL REMOVE ALL CONTAINERS from your system.");
         IncusClient = new LocalUnixSocketIncusClient();
         TestHelper.RemoveAllContainers(IncusClient).Wait();
     }
